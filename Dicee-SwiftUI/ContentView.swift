9e265ae6f5ec2cf4d10e4a0ace1d7b39d1cd95c0
Richard Garrison <azmobiledev@gmail.com>
2019-11-07 21:37:32 -0700

    Initial Commit

---
+++ Dicee-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dicee-SwiftUI
+//
+//  Created by Richard Garrison on 11/7/19.
+//  Copyright © 2019 ios-mobile.dev. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
